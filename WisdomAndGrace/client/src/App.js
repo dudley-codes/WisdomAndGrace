9f2e838274ccfea124308ab38b29b3e97a64e51e
dudley-codes <alexdudley@me.com>
2021-07-22 13:58:37 -0500

    fixed auth bug

--- WisdomAndGrace/client/src/App.js
+++ WisdomAndGrace/client/src/App.js
@@ -7,7 +7,7 @@ import ApplicationViews from "./components/ApplicationViews";
 import { onLoginStatusChange } from "./modules/authManager";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [ isLoggedIn, setIsLoggedIn ] = useState(null);
 
   useEffect(() => {
     onLoginStatusChange(setIsLoggedIn);
@@ -17,13 +17,13 @@ function App() {
   //  Then it will be set to true or false by the "onLoginStatusChange" function
   if (isLoggedIn === null) {
     // Until we know whether or not the user is logged in or not, just show a spinner
-    return <Spinner className="app-spinner dark"/>;
+    return <Spinner className="app-spinner dark" />;
   }
 
   return (
     <Router>
-        <Header isLoggedIn={isLoggedIn} />
-        <ApplicationViews isLoggedIn={isLoggedIn} />
+      <Header isLoggedIn={ isLoggedIn } />
+      <ApplicationViews isLoggedIn={ isLoggedIn } />
     </Router>
   );
 }
